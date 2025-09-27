99d409be27b264bb01b7c379050634f5a52c161b
unknown <pithandev>
2025-09-27 13:51:24 -0300

    get is done and trying to fix update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -18,7 +19,8 @@ func main() {
 		fmt.Println()
 		fmt.Println("TRY SAVE SOMETHING IN TXT FILE.")
 		fmt.Println("1) Save.")
-		fmt.Println("2) Show data.")
+		fmt.Println("2) Show.")
+		fmt.Println("3) Update.")
 		fmt.Println("0) QUIT!!!")
 		fmt.Println()
 
@@ -27,15 +29,46 @@ func main() {
 
 		switch opt {
 		case "1":
+			fmt.Println("==============================")
+			fmt.Println("              SAVE             ")
+			fmt.Print("Type: ")
 
 			textScan := bufio.NewScanner(os.Stdin)
 			textScan.Scan()
 
 			text := textScan.Text()
 
+			fmt.Println("==============================")
+
 			save(text)
 		case "2":
+			fmt.Println("==============================")
+			fmt.Println("         SHOWING ALL          ")
+
 			fmt.Println(getAll())
+			fmt.Println("==============================")
+
+		case "3":
+			fmt.Println("==============================")
+			fmt.Println("            UPDATE           ")
+
+			fmt.Println("ID: ")
+			idScan := bufio.NewScanner(os.Stdin)
+			idScan.Scan()
+			id, err := strconv.ParseInt(idScan.Text(), 10, 64)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("UPDATE: ")
+			textScan := bufio.NewScanner(os.Stdin)
+			textScan.Scan()
+			newText := textScan.Text()
+
+			update(int(id), newText)
+			fmt.Println("==============================")
+
 		case "0":
 			quit = true
 		}
@@ -69,4 +102,22 @@ func getAll() string {
 	}
 
 	return string(readFile)
+}
+
+func update(id int, newText string) bool {
+
+	updateFile, err := os.OpenFile("storage.txt", os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	updateFile.Seek(int64(id), 0)
+
+	updateFile.Write([]byte(newText))
+
+	defer updateFile.Close()
+
+	return true
 }
